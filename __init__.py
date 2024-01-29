be308113277d2639466183abe93514720938bf06
ericmarti05 <145457556+ericmarti05@users.noreply.github.com>
2024-01-29 08:52:17 +0100

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+def comprova_signe(num: int)->int:
+  
+    resultat = 0
+    if(num>0):
+                resultat = 1
+   
+    elif(num==0):
+        resultat = 0
+    
+    else:
+        resultat = -1
+        
+    return resultat
